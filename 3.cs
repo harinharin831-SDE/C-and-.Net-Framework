c8ee4bc0411bc7ffcb0c6bc6c9d1ba07dabd7287
harinharin831-SDE <harinharin831@gmail.com>
2026-09-13 16:07:21 +0530

    Add files via upload

---
+++ 3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+// Base Class 
+class Person 
+{ 
+ public void ShowPerson() 
+ { 
+ Console.WriteLine("Person Details");
+ } 
+} 
+ 
+class Student : Person 
+{ 
+ public void ShowStudent() 
+ { 
+ Console.WriteLine("Student Details");
+ } 
+} 
+
+interface ISports 
+{ 
+ void ShowSports();
+} 
+
+class Result : Student, ISports 
+{
+public void ShowResult() 
+ { 
+ Console.WriteLine("Result Published");
+ } 
+ public void ShowSports() 
+ { 
+ Console.WriteLine("Sports Certificate Awarded");
+ } 
+} 
+class Program 
+{ 
+ static void Main(string[] args) 
+ { 
+ Result obj = new Result();
+ obj.ShowPerson();
+ obj.ShowStudent();
+ obj.ShowSports();
+ obj.ShowResult();
+ Console.ReadKey();
+ } 
+}
